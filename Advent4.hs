e8a00f6f1c1482f1a40d984f7b111c33daa4a7cb
mike-k-houghton <mike_k_houghton@yahoo.co.uk>
2016-12-13 20:53:24 +0000

    done (could be better!)

--- Advent4.hs
+++ Advent4.hs
@@ -3,7 +3,7 @@ import Data.Char
 import Data.Ord
 import Data.List.Utils
 import Data.Function (on)
-import Data.List (sortBy)
+import Data.List (sortBy, elemIndex)
 import Data.Map as Map (toList, fromListWith)
 -- ["aczupnetwp-dnlgpyrpc-sfye-dstaatyr-561[patyc]","jsehsyafy-vqw-ljsafafy-866[nymla]","tyepcyletzylw-ncjzrpytn-prr-opawzjxpye-743[cnrdl]","foadouwbu-qvcqczohs-obozmgwg-662[lamjh]","ckgvutofkj-pkrrehkgt-zkinturume-436[krtue]","pelbtravp-pnaql-ernpdhvfvgvba-481[szram]","yflexwxoalrp-ciltbo-tlohpelm-887[bmwep]"]
 -- Each room consists of an encrypted name (lowercase letters separated by dashes) 
@@ -16,20 +16,37 @@ type CodedRoom     = String
 type DecodedName   = String
 type SectorID      = String 
 type Checksum      = String
+type DecryptedName = String
 type AllCodedRooms = [CodedRoom]
-type DecodedRoom   = (DecodedName, SectorID, Checksum)
+type DecodedRoom   = (DecodedName, SectorID, Checksum, DecryptedName)
  
+-- replaceEach :: String -> String -> String
+-- replaceEach o n s = map f
+
+letters :: [Char]
+letters = ['a'..'z']
+
+rot :: Int -> Char  -> Char
+rot _ '-' = ' '
+rot n c  = 
+  case (elemIndex c letters) of
+   Nothing ->  c
+   Just i  -> (!!) letters  ((i + n) `mod` 26)  
+
+splitOnPredicate :: (a -> Bool) -> [a] -> ([a], [a])
+splitOnPredicate f xs = (takeWhile f xs, dropWhile f xs )
+
 decode :: CodedRoom -> DecodedRoom
 decode cr = room where
-   
-   (decodeName' , r) = (replace "-" "" $ takeWhile (not . isDigit) cr , dropWhile (not . isDigit) cr)
-   (sectorId, checkSum) = (takeWhile isDigit r, replace "]" "" $ replace "[" "" $ dropWhile isDigit r)
-   tupleList = sortTupleList $ toList $ fromListWith (+) [(c, 1) | c <- decodeName']
+--northpole object storage   
+   (decodeName' , rhs)  = splitOnPredicate (not . isDigit) cr
+   (sectorId, checkSum) = splitOnPredicate isDigit rhs
+   tupleList = sortTupleList $ toList $ fromListWith (+) [(c, 1) | c <- replace "-" ""  decodeName']
    decodeName = (take 5) $ foldl (\ac x  -> ac ++ [(fst x)]) "" tupleList
-   room = (decodeName, sectorId, checkSum)
+   room = (decodeName, sectorId, checkSum, map (rot ((read sectorId)::Int)) decodeName' )
 
 secId :: DecodedRoom -> Int
-secId (decodedName, secId, checksum) =(read secId)::Int
+secId (decodedName, secId, checksum, _) =(read secId)::Int
 
 splitOnPred ::(a -> Bool) -> [a] -> ([a], [a])
 splitOnPred f as = (takeWhile f as, dropWhile f as)
@@ -46,10 +63,12 @@ sortTupleList lst = reverse $ sortBy tupleOrder lst where
           | x1 > x2 = GT
 
 roomReal :: DecodedRoom -> Bool
-roomReal (decodedName, secId, checksum) = decodedName == checksum
+roomReal (decodedName, secId, checksum, _) = "[" ++ decodedName ++ "]" == checksum
 
 addIds :: DecodedRoom -> Int -> Int
-addIds (decodedName, secId, checksum) acc = acc + (read secId)::Int
+addIds (decodedName, secId, checksum, _) acc = acc + (read secId)::Int
+checkForString :: String -> String -> Bool
+checkForString t s = contains t s 
 
 evaluate :: AllCodedRooms -> Int
 evaluate crs = foldr addIds 0 $ filter roomReal (map decode crs)
@@ -58,4 +77,4 @@ main :: IO ()
 main = do
   
   inpt <- readFile "in4.txt"
-  print $ evaluate (lines inpt)
+  print $ evaluate  (lines inpt)
