e08c064d1248e8a7e9ea1a51a43da3f435d70cbd
mike-k-houghton <mike_k_houghton@yahoo.co.uk>
2016-12-14 19:58:11 +0000

    added

--- Advent5_Part2.hs
+++ Advent5_Part2.hs
@@ -2,10 +2,11 @@
 import Data.Char
 import Data.Digest.Pure.MD5
 import Data.ByteString.Lazy.Char8 hiding (map, length, take, drop, elem)
+import Text.Read
 
-type Pos = Char
+type Pos = Int
 fiveZerosChar :: [Char] -> Maybe (Pos, Char)
-fiveZerosChar ('0':'0':'0':'0':'0':pos:c:_) = Just (pos, c)
+fiveZerosChar ('0':'0':'0':'0':'0':pos:c:_) = Just (digitToInt pos, c)
 fiveZerosChar _ = Nothing
 
 md5Hash :: String -> String
@@ -14,16 +15,30 @@ md5Hash  = show . md5 . pack
 allDone :: String -> Bool
 allDone  = not . elem '*' 
 
-findACharLoc :: Int -> String -> (Pos, Char)
-findACharLoc ix text = 
+findACharAndLoc :: Int -> String -> (Pos, Char, Int )
+findACharAndLoc ix text = 
     case fiveZerosChar md of
-        Nothing -> findACharLoc (ix + 1) text
-        Just (p, c) -> (p, c)
+        Nothing -> findACharAndLoc (ix + 1) text
+        Just (p, c) -> if p <= 7 then (p, c, ix) else findACharAndLoc (ix + 1) text
         where md = md5Hash (text ++ show ix)
 
 
-findPassword :: String -> String
-findPassword = undefined
+fillLocWithChar :: Char -> Int -> String -> String
+fillLocWithChar c loc target 
+  | target !! loc /= '*' = target -- already been done
+  | otherwise = take loc target ++ [c] ++ drop (loc + 1) target
 
-main = print $ md5 "Hello, world!"
+
+findPassword' :: Bool -> Int -> String -> String -> String
+findPassword' True _ partial _ = partial
+findPassword' _ ix partial txt = findPassword' isDone (ix' + 1) partial' txt where
+    (pos, ch, ix') = findACharAndLoc ix txt
+    partial' = fillLocWithChar ch pos partial
+    isDone = allDone partial'
+
+findPassword :: String -> String
+findPassword txt = findPassword' False 0 "********" txt
+
+-- Just run main
+-- main = print $ findPassword "reyedfim"
 
