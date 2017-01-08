cacff4bdf8da50636316a61e10edc1ee8f68e1b8
mike-k-houghton <mike_k_houghton@yahoo.co.uk>
2017-01-08 22:26:59 +0000

    changed

--- Advent14.hs
+++ Advent14.hs
@@ -1,27 +1,47 @@
 
 {-# LANGUAGE OverloadedStrings #-}
 {-# OPTIONS -Wall -fwarn-tabs -fno-warn-type-defaults -fno-warn-unused-do-bind #-}
--- import Data.Digest.Pure.MD5
--- import Data.ByteString.Lazy.Char8 
-import Control.Monad
+import Data.List (nub, tails, null)
+import Data.Hash.MD5 (md5s, Str(..))
 
-matches :: (Char, String) -> Maybe (Char, String)
-matches (_, []) = Nothing
-matches (c, (x:xs)) = if c == x then Just (c, xs) else Nothing
-    
-dbl  :: String -> Maybe (Char, String)
-dbl []     = Nothing
-dbl [_]    = Nothing
-dbl (a:b:[]) = if a == b then Just (a, []) else Nothing
-dbl (a:b:xs) 
-    | a == b = Just (a, xs)
-    | otherwise = dbl (b:xs)
+salt = "ihaygndm"
+-- salt = "abc"
 
-nPl :: Int -> [Char] -> Maybe (Char, String)
-nPl n s | length s < n = Nothing
-nPl n s  = applyn (n - 3) (dbl s >>= matches) where 
-        applyn :: Int -> Maybe (Char, String) -> Maybe (Char, String)
-        applyn n' m' = m' >>= foldr (<=<) return (replicate n' matches)
-          
 
-main = print "" -- print $ md5 "Hello, world!"
+
+part1 :: IO ()
+part1 = print $ head $ drop 63 $ filter (\i -> maybeKey sq i && verifyKey sq i) [0..]
+    where sq = md5List
+
+part2 :: IO ()
+part2 = print $ head $ drop 63 $ filter (\i -> maybeKey sq i && verifyKey sq i) [0..]
+    where sq = md5ListStretched
+
+md5List :: [String]
+md5List = [makeMd5 i | i <- [0..]]
+    where makeMd5 i = md5s (Str (salt ++ show i))
+
+md5ListStretched :: [String]
+md5ListStretched = [makeMd5 i | i <- [0..]]
+    where makeMd5 i = stretch $ md5s (Str (salt ++ show i)) 
+          stretch h0 = foldr (\_ h -> md5s (Str h)) h0 [1..2016]
+
+maybeKey :: [String] -> Int-> Bool
+maybeKey s = not . null . repeatN 3 . ((!!) s)
+
+verifyKey :: [String] -> Int -> Bool
+verifyKey s i = any (confirmation) $ take 1000 $ drop (i+1) s
+    where c = head $ repeatN 3 $ s!!i
+          confirmation m = c `elem` (repeatN 5 m)
+
+repeatN :: Int -> String -> [String]
+repeatN n = filter (null . tail) . map (nub) . subStr n
+
+subStr :: Int -> [a] -> [[a]]
+subStr l = filter (\s -> (length s) == l) . map (take l) . tails
+
+
+main :: IO ()
+main = do 
+        part1 
+        part2
